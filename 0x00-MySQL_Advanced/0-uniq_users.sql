6b1946ede8935a58a50b719f4a423cd23fee2cd5
clencyc <clency2023@gmail.com>
2024-08-08 22:42:27 +0000

    We are all unique

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `users` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`email` varchar(255) NOT NULL UNIQUE,
+`name` varchar(255) DEFAULT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
